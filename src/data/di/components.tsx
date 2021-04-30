57eb1a988e3a0470c87433b1d17d46effb583efe
JMRJ <jmramos@grupoasesores.com.mx>
2021-04-30 17:04:32 -0600

    New CoinCapComponent implementation

---
+++ src/data/di/components.tsx
@@ -0,0 +1,16 @@
+import axios from 'axios';
+import GetAssetsUseCase from '../../domain/use_cases/get_assets_use_case';
+import { generateGetAssetsUseCase } from '../di/modules';
+
+const axiosInstance = axios.create({
+  baseURL: 'https://api.coincap.io/v2/',
+  timeout: 6000,
+});
+
+export interface CoinCapComponent {
+  getAssetsUseCase: GetAssetsUseCase;
+}
+
+export const appComponent: CoinCapComponent = {
+  getAssetsUseCase: generateGetAssetsUseCase(axiosInstance),
+};
---
+++ src/data/di/modules.tsx
@@ -0,0 +1,16 @@
+import { AxiosInstance } from 'axios';
+import CoinCapDataSource from '../../domain/data_sources/coin_cap_data_source';
+import CoinCapRepository from '../../domain/repositories/coin_cap_repository';
+import GetAssetsUseCase from '../../domain/use_cases/get_assets_use_case';
+import CoinCapDataSourceImpl from '../data_sources/coin_cap_data_source_impl';
+import CoinCapRepositoryImpl from '../repositories/coin_cap_repository_impl';
+
+export const generateGetAssetsUseCase = (
+  axiosInstance: AxiosInstance,
+): GetAssetsUseCase => {
+  const dataSource: CoinCapDataSource = new CoinCapDataSourceImpl(
+    axiosInstance,
+  );
+  const repository: CoinCapRepository = new CoinCapRepositoryImpl(dataSource);
+  return new GetAssetsUseCase(repository);
+};
